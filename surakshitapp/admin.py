7095cac93ea83b4e113daf33062716c2b64644d5
Prajjwall123 <pokhrelprajwal29@gmail.com>
2023-12-23 15:12:19 +0545

    adding models

--- surakshitapp/admin.py
+++ surakshitapp/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ram
+from .models import Earthquake, Flood, Glof, Landslide, Ram, User
+
 admin.site.register(Ram)
+admin.site.register(User)
+admin.site.register(Earthquake)
+admin.site.register(Flood)
+admin.site.register(Landslide)
+admin.site.register(Glof)
+
--- surakshitapp/models.py
+++ surakshitapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-from django.db import models
 
 class Ram(models.Model):
     capacity = models.PositiveIntegerField()
@@ -9,3 +8,100 @@ class Ram(models.Model):
 
     def __str__(self):
         return f"{self.capacity}GB {self.manufacturer} RAM"
+
+#USERS
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=1000)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    profile = models.ImageField(upload_to='static/profiles/',null=True,blank=True)
+    #latlong values
+    latitude=models.FloatField(null=True,blank=True)
+    latitude=models.FloatField(null=True,blank=True)
+    # location = models.PointField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user_id
+
+# #DISASTERS
+
+class Earthquake(models.Model):
+    earthquake_key=models.AutoField(primary_key=True)
+    richter=models.FloatField(null=True,blank=True)
+    year=models.IntegerField()
+    casualties=models.IntegerField(null=True,blank=True)
+    loss=models.IntegerField(null=True,blank=True)
+    #latlong values
+    latitude_epicenter=models.FloatField(null=True,blank=True)
+    longitude_epicenter=models.FloatField(null=True,blank=True)
+    # epicenter = models.PointField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.earthquake_key
+
+class Flood(models.Model):
+    FLOOD_CAUSES = [
+        ('rainfall', 'Rainfall'),
+        ('landslide', 'Landslide'),
+        ('golf', 'Golf'),
+        ('dam_failure', 'Dam Failure'),
+        ('river_channel_changes', 'River Channel Changes'),
+        ('other', 'Other'),
+    ]
+    flood_key=models.AutoField(primary_key=True)
+    rainfall=models.FloatField(null=True,blank=True)
+    year=models.IntegerField()
+    casualties=models.IntegerField(null=True,blank=True)
+    loss=models.IntegerField(null=True,blank=True)
+    latitude_epicenter=models.FloatField(null=True,blank=True)
+    longitude_epicenter=models.FloatField(null=True,blank=True)
+    # epicenter = models.PointField(null=True, blank=True)
+    radius = models.FloatField(null=True, blank=True)
+    cause = models.CharField(max_length=100, choices=FLOOD_CAUSES, default='rainfall')
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.flood_key
+
+
+class Glof(models.Model):
+    GLOF_CAUSES= [
+        ('avalanche', 'Avalanche'),
+        ('earthquake', 'Earthquake'),
+        ('dam_failure', 'Dam Failure'),
+        ('other', 'Other'),
+    ]
+    glof_key=models.AutoField(primary_key=True)
+    water_level=models.FloatField(null=True,blank=True)
+    year=models.IntegerField()
+    casualties=models.IntegerField(null=True,blank=True)
+    cause = models.CharField(max_length=100, choices=GLOF_CAUSES, default='avalanche')
+    loss=models.IntegerField(null=True,blank=True)
+    latitude_epicenter=models.FloatField(null=True,blank=True)
+    longitude_epicenter=models.FloatField(null=True,blank=True)
+    # epicenter = models.PointField(null=True, blank=True)
+    radius = models.FloatField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.glof_key
+
+class Landslide(models.Model):
+    LANDSLIDE_CAUSES= [
+        ('earthquake', 'Earthquake'),
+        ('rainfall', 'Rainfall'),
+        ('soil_erosion', 'Soil Erosion'),
+        ('other', 'Other'),
+    ]
+    landslide_key=models.AutoField(primary_key=True)
+    year=models.IntegerField()
+    casualties=models.IntegerField(null=True,blank=True)
+    loss=models.IntegerField(null=True,blank=True)
+    cause = models.CharField(max_length=100, choices=LANDSLIDE_CAUSES, default='rainfall')
+    latitude_epicenter=models.FloatField(null=True,blank=True)
+    longitude_epicenter=models.FloatField(null=True,blank=True)
+    # epicenter = models.PointField(null=True, blank=True)
+    radius = models.FloatField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.earthquake_key
